a0d705d34a54b57b2ab046eb5a406b3934264655
James Shephard <james_shephard@hotmail.com>
2021-02-04 13:38:39 +0000

    Moves old stuff out Makes new full screen image shader thing

---
+++ Assets/CamCloud.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCloud : MonoBehaviour
+{
+    public Material mat;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Debug.Log("Hello!");
+        Graphics.Blit(src, dest, mat);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
