60c4e2f7cec3b7a7fa0ae4f2d3913f1a78c1d750
James Shephard <james_shephard@hotmail.com>
2021-02-07 02:48:17 +0000

    Computer shader for generating an SDF

---
+++ Assets/SdfGen/SdfDebug.shader
@@ -0,0 +1,57 @@
+﻿Shader "Unlit/SdfDebug"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _SdfScale ("SDF Scale", Range(1, 128)) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float _SdfScale;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                // sample the texture
+                float3 inv = float3(1.0f, 1.0f, 1.0f) - (tex2D(_MainTex, i.uv).xyz / _SdfScale);
+                fixed4 col = fixed4(inv, 1.0f);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/SdfGen/SdfGen.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SdfGen : MonoBehaviour
+{
+    public ComputeShader computeShader;
+
+    private RenderTexture renderTexture;
+
+    private int kernel;
+
+    private int resultId;
+    private int centresId;
+    private int numCentresId;
+    private int sizeId;
+
+    private const int Width = 128;
+    private const int Height = 128;
+    private const int NumCentres = 8;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kernel = computeShader.FindKernel("GenSdf");
+        resultId = Shader.PropertyToID("Result");
+        centresId = Shader.PropertyToID("Centres");
+        numCentresId = Shader.PropertyToID("NumCentres");
+        sizeId = Shader.PropertyToID("Size");
+
+        renderTexture = new RenderTexture(Width, Height, 0, RenderTextureFormat.ARGBFloat);
+        renderTexture.enableRandomWrite = true;
+        renderTexture.wrapMode = TextureWrapMode.Repeat;
+        renderTexture.Create();
+
+        GetComponent<Renderer>().material.mainTexture = renderTexture;
+
+        GenSdf();
+    }
+
+    void GenSdf()
+    {
+        List<Vector3> centresList = new List<Vector3>();
+
+        for(int i=0; i<NumCentres; i++) {
+            centresList.Add(new Vector3(Random.Range(0, Width), Random.Range(0, Height), 0));
+        }
+
+        Vector3[] centres = centresList.ToArray();
+
+        ComputeBuffer buffer = new ComputeBuffer(centres.Length, sizeof(float)*3);
+        buffer.SetData(centres);
+
+        computeShader.SetBuffer(kernel, centresId, buffer);
+        computeShader.SetVector(sizeId, new Vector4(Width, Height, 0, 0));
+        computeShader.SetTexture(kernel, resultId, renderTexture);
+        computeShader.SetInt(numCentresId, centres.Length);
+        computeShader.Dispatch(kernel, Width/8, Height/8, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
