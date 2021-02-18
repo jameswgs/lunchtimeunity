05eb33c5f3b2531c64c677ac3b9694e73702c0ae
James Shephard <james_shephard@hotmail.com>
2021-02-18 21:30:08 +0000

    Pulls cloud texture genrator out into separate class

--- Assets/CamCloud.cs
+++ Assets/CamCloud.cs
@@ -4,65 +4,89 @@ using UnityEngine;
 
 public class CamCloud : MonoBehaviour
 {
-    public Material mat;
-    public GameObject cloud;
+    class CloudTexture
+    {
+        public ComputeShader computeShader;
+        public int numCentres = 128;
+        public float cloudSize = 20.0f;
+        public int cloudOctaves = 4;
+
+        private RenderTexture renderTexture;
+
+        private const int Width = 128;
+        private const int Height = 128;
+        private const int Depth = 128;
+
+        public void GenClouds()
+        {
+            renderTexture = new RenderTexture(Width, Height, 0, RenderTextureFormat.ARGBFloat);
+            renderTexture.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+            renderTexture.volumeDepth = Depth;
+            renderTexture.enableRandomWrite = true;
+            renderTexture.wrapMode = TextureWrapMode.Repeat;
+            renderTexture.Create();
+
+            List<Vector3> centresList = new List<Vector3>();
+
+            Random.seed = 0;
+
+            for (int i = 0; i < numCentres; i++)
+            {
+                centresList.Add(new Vector3(Random.Range(0, Width), Random.Range(0, Height), Random.Range(0, Depth)));
+            }
+
+            Vector3[] centres = centresList.ToArray();
+
+            ComputeBuffer centresBuffer = new ComputeBuffer(centres.Length, sizeof(float) * 3);
+            centresBuffer.SetData(centres);
+
+            int kernel = computeShader.FindKernel("GenSdf");
+            computeShader.SetBuffer(kernel, Shader.PropertyToID("Centres"), centresBuffer);
+            computeShader.SetVector(Shader.PropertyToID("Size"), new Vector4(Width, Height, Depth, 0));
+            computeShader.SetTexture(kernel, Shader.PropertyToID("Result"), renderTexture);
+            computeShader.SetInt(Shader.PropertyToID("NumCentres"), centres.Length);
+            computeShader.SetInt(Shader.PropertyToID("NumOctaves"), cloudOctaves);
+            computeShader.SetFloat(Shader.PropertyToID("BaseRadius"), cloudSize);
+            computeShader.Dispatch(kernel, Width / 8, Height / 8, Depth / 8);
+
+            centresBuffer.Dispose();
+        }
+
+        public RenderTexture GetCloudTexture()
+        {
+            return renderTexture;
+        }
+
+    }
+
+    public Material material;
     public ComputeShader computeShader;
-    public float CloudSize = 20.0f;
-    public int CloudOctaves = 4;
+    public int numCentres = 128;
+    public float cloudSize = 20.0f;
+    public int cloudOctaves = 4;
 
-    private RenderTexture renderTexture;
-
-    private const int Width = 128;
-    private const int Height = 128;
-    private const int Depth = 128;
-    private const int NumCentres = 128;
+    private CloudTexture cloudTexture = new CloudTexture();
 
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
         GenClouds();
     }
 
     public void GenClouds()
-    { 
-        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
-
-        renderTexture = new RenderTexture(Width, Height, 0, RenderTextureFormat.ARGBFloat);
-        renderTexture.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
-        renderTexture.volumeDepth = Depth;
-        renderTexture.enableRandomWrite = true;
-        renderTexture.wrapMode = TextureWrapMode.Repeat;
-        renderTexture.Create();
-
-        List<Vector3> centresList = new List<Vector3>();
-
-        for (int i = 0; i < NumCentres; i++)
-        {
-            centresList.Add(new Vector3(Random.Range(0, Width), Random.Range(0, Height), Random.Range(0, Depth)));
-        }
-
-        Vector3[] centres = centresList.ToArray();
-
-        ComputeBuffer centresBuffer = new ComputeBuffer(centres.Length, sizeof(float) * 3);
-        centresBuffer.SetData(centres);
-
-        int kernel = computeShader.FindKernel("GenSdf");
-        computeShader.SetBuffer(kernel, Shader.PropertyToID("Centres"), centresBuffer);
-        computeShader.SetVector(Shader.PropertyToID("Size"), new Vector4(Width, Height, Depth, 0));
-        computeShader.SetTexture(kernel, Shader.PropertyToID("Result"), renderTexture);
-        computeShader.SetInt(Shader.PropertyToID("NumCentres"), centres.Length);
-        computeShader.SetInt(Shader.PropertyToID("NumOctaves"), CloudOctaves);
-        computeShader.SetFloat(Shader.PropertyToID("BaseRadius"), CloudSize);
-        computeShader.Dispatch(kernel, Width / 8, Height / 8, Depth / 8);
-
-        centresBuffer.Dispose();
+    {
+        cloudTexture.computeShader = computeShader;
+        cloudTexture.numCentres = numCentres;
+        cloudTexture.cloudSize = cloudSize;
+        cloudTexture.cloudOctaves = cloudOctaves;
+        cloudTexture.GenClouds();
     }
 
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        mat.SetTexture("_CloudTexture", renderTexture);
-        mat.SetVector("_CloudPosition", cloud.transform.position);
-        Graphics.Blit(src, dest, mat);
+        material.SetTexture("_CloudTexture", cloudTexture.GetCloudTexture());
+        Graphics.Blit(src, dest, material);
     }
 
     // Update is called once per frame
